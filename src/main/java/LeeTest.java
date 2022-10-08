c44f822789e161179cada37900518142614004fb
Polobear9 <lieams1996@gmail.com>
2022-10-09 00:07:50 +0900

    IntelliJ Pull Request Test. Make a PR use a IntelliJ.

--- src/main/java/LeeTest.java
+++ src/main/java/LeeTest.java
@@ -1,5 +1,5 @@
 public class LeeTest {
     public static void main(String[] args) {
-        System.out.println("hello world!");
+        System.out.println("Lee intelliJ Branch Test Use a IDE , Make a PR");
     }
 }
